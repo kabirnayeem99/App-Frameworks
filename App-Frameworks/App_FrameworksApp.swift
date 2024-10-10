a78548c0202c42558c39fdb8eb8e4f407b020687
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-10-10 21:16:24 +0600

    Initial Commit

---
+++ App-Frameworks/App_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_FrameworksApp.swift
+//  App-Frameworks
+//
+//  Created by Naimul Kabir on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct App_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
